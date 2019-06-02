199c4912b750162a2b2c12b4f3ce4edda10af5ab
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2019-06-02 16:58:43 +0200

    Compile without SDL support & some fixes and optimizations

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 FFMPEG_VERSION=4.1.3
 
-rm -rf ffmpeg-${FFMPEG_VERSION} ffmpeg-${FFMPEG_VERSION}.tar.bz2*
+rm -rf ffmpeg-${FFMPEG_VERSION}*
 wget https://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2
 tar xjf ffmpeg-${FFMPEG_VERSION}.tar.bz2
 cd ffmpeg-${FFMPEG_VERSION}
@@ -11,31 +11,34 @@ echo "Configure"
 
 # Configure and compile LLVM bitcode
 emconfigure ./configure \
-  --arch=wasm32 \
   --ar=emar \
   --cc=emcc \
   --cxx=em++ \
+  --disable-asm \
+  --disable-autodetect \
+  --disable-doc \
+  --disable-sdl2 \
   --disable-stripping \
-  --disable-x86asm \
   --enable-cross-compile || exit $?
-  # --disable-autodetect
 
 echo "Build"
-emmake make || exit $?
+emmake make -j8 || exit $?
 
 # Generate `.wasm` file
 echo "Link"
-mv ffmpeg ffmpeg.o
-emcc ffmpeg.o -o ../ffmpeg.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=52428800
+TOTAL_MEMORY=67108864  # increase heap size from 16mb to 64mb
 
-mv ffplay ffplay.o
-emcc ffplay.o -o ../ffplay.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=52428800
+mv ffmpeg ffmpeg.o
+emcc ffmpeg.o -o ../ffmpeg.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=${TOTAL_MEMORY}
+
+# mv ffplay ffplay.o
+# emcc ffplay.o -o ../ffplay.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=${TOTAL_MEMORY}
 
 mv ffprobe ffprobe.o
-emcc ffprobe.o -o ../ffprobe.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=52428800
+emcc ffprobe.o -o ../ffprobe.wasm -s ERROR_ON_UNDEFINED_SYMBOLS=0 -s TOTAL_MEMORY=${TOTAL_MEMORY}
 
 echo "Clean"
 cd ..
-rm -rf ffmpeg-${FFMPEG_VERSION} ffmpeg-${FFMPEG_VERSION}.tar.gz
+rm -rf ffmpeg-${FFMPEG_VERSION}*
 
 echo "Done"
